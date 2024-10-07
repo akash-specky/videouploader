021a6ab51d65586681e401ac1cdc437ee926aa2e
robin.raj@duronto.in <robin.raj@duronto.in>
2024-10-07 16:55:12 +0530

    delete chunks videos

--- src/main/java/com/example/videouploader/utility/Constant.java
+++ src/main/java/com/example/videouploader/utility/Constant.java
@@ -8,17 +8,5 @@ public class Constant {
     public static final String COMBINED_VIDEO_DIR = "C:/Users/akash/OneDrive/Desktop/Uploaded Video/combined";
 
 
-    public static void deleteFileOrDirectory(File file) {
-
-        if (file.isDirectory()) {
-            File[] allContents = file.listFiles();
-            if (allContents != null) {
-                for (File f : allContents) {
-                    deleteFileOrDirectory(f);
-                }
-            }
-        }
-        file.delete();
-    }
 
 }
