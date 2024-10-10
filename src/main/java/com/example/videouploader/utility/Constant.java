0f5375e8cc2b390d796b0643a5c7505f4e8b3a00
robin.raj@duronto.in <robin.raj@duronto.in>
2024-10-10 18:21:35 +0530

    add temp path

--- src/main/java/com/example/videouploader/utility/Constant.java
+++ src/main/java/com/example/videouploader/utility/Constant.java
@@ -1,11 +1,17 @@
 package com.example.videouploader.utility;
 
 import java.io.File;
+import java.nio.file.Paths;
 
 public class Constant {
-    public static final String UPLOAD_DIR = "C:/Users/akash/OneDrive/Desktop/Uploaded Video";
-    public static final String CHUNK_DIR = "C:/Users/akash/OneDrive/Desktop/Uploaded Video/chunks";
-    public static final String COMBINED_VIDEO_DIR = "C:/Users/akash/OneDrive/Desktop/Uploaded Video/combined";
+//    public static final String UPLOAD_DIR = "C:/Users/akash/OneDrive/Desktop/Uploaded Video";
+//    public static final String CHUNK_DIR = "C:/Users/akash/OneDrive/Desktop/Uploaded Video/chunks";
+//    public static final String COMBINED_VIDEO_DIR = "C:/Users/akash/OneDrive/Desktop/Uploaded Video/combined";
+public static final String BASE_DIR = System.getProperty("java.io.tmpdir");
+
+    public static final String UPLOAD_DIR = Paths.get(BASE_DIR, "UploadedVideo").toString();
+    public static final String CHUNK_DIR = Paths.get(UPLOAD_DIR, "chunks").toString();
+    public static final String COMBINED_VIDEO_DIR = Paths.get(UPLOAD_DIR, "combined").toString();
 
 
 
