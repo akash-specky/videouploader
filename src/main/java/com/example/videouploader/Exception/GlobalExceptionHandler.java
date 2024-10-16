470be6864a4f142448f2abd021dd390e5d06a4f0
robin.raj@duronto.in <robin.raj@duronto.in>
2024-10-16 13:37:52 +0530

    removed duplicate classes

--- src/main/java/com/example/videouploader/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/videouploader/Exception/GlobalExceptionHandler.java
@@ -1,18 +1,25 @@
 package com.example.videouploader.Exception;
 
 
+import com.example.videouploader.dtos.CommonResponseDTO;
+import com.example.videouploader.exceptions.UserAlreadyExist;
 import com.mongodb.MongoException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(VideoException.class)
     public ResponseEntity<MyErrorDetails> UsgerException(VideoException ue, WebRequest wr){
@@ -62,6 +69,12 @@ public class GlobalExceptionHandler {
 
         return new  ResponseEntity<MyErrorDetails>(error, HttpStatus.BAD_REQUEST);
     }
+    @ExceptionHandler(UserAlreadyExist.class)
+    public final ResponseEntity<CommonResponseDTO> handleResourceConflictExceptions(Exception ex) {
 
+        log.error(ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(new CommonResponseDTO(false, ex.getMessage(), null));
+    }
 
 }
