99e68bd3307f1dcda7c3fdeeece8476f407ed965
robin.raj@duronto.in <robin.raj@duronto.in>
2024-10-22 15:39:45 +0530

    fixed sso

--- src/main/java/com/example/videouploader/security/SecurityConfig.java
+++ src/main/java/com/example/videouploader/security/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.example.videouploader.security;
 
 import com.example.videouploader.serviceImpl.GoogleTokenValidator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -26,26 +30,31 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.addFilterBefore(new TokenValidationFilter(googleTokenValidator), UsernamePasswordAuthenticationFilter.class);
 
-        http
+        http.csrf().disable()
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/api/v1/users/login", "/v1/api/logout").permitAll()
+                                .anyRequest().authenticated()
 
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers(urls).permitAll()
-                        .anyRequest().authenticated()
                 )
-                .oauth2Login(oauth2 -> oauth2
-                        .defaultSuccessUrl("/v1/users/login", true)
-                )
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .logout(LogoutConfigurer::permitAll);
+                .oauth2Login().defaultSuccessUrl("/videos/hello")
+                .and().oauth2ResourceServer().jwt();
 
         return http.build();
     }
 
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        String jwkSetUri = "https://www.googleapis.com/oauth2/v3/certs";
+        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
+    }
+
+//    @Bean
+//    public OpaqueTokenIntrospector opaqueTokenIntrospector() {
+//        return new SpringOpaqueTokenIntrospector(
+//                "http://localhost:8092/api/v1/users/login", googleClientId, googleClientSecret);
+//    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
